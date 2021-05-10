b4aed6a1db854cc34ce7a25a5e8a5f4628d31c38
Juan <juloespares@yahoo.com.au>
2021-05-10 19:53:59 +1000

    started read/load

--- src/towersim/control/ControlTowerInitialiser.java
+++ src/towersim/control/ControlTowerInitialiser.java
@@ -1,11 +1,20 @@
 package towersim.control;
 
 import towersim.aircraft.Aircraft;
+import towersim.aircraft.AircraftCharacteristics;
+import towersim.aircraft.FreightAircraft;
+import towersim.aircraft.PassengerAircraft;
+import towersim.ground.Gate;
 import towersim.ground.Terminal;
+import towersim.tasks.Task;
+import towersim.tasks.TaskList;
+import towersim.tasks.TaskType;
 import towersim.util.MalformedSaveException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +43,22 @@ public class ControlTowerInitialiser {
      * @throws IOException if an IOException is encountered when reading from the reader
      */
     public static long loadTick(Reader reader) throws MalformedSaveException, IOException {
-        return 0;
+        if (reader == null) {
+            throw new IOException();
+        }
+        BufferedReader file = new BufferedReader(reader);
+        long ticksElapsed;
+        try {
+            ticksElapsed = Long.parseLong(file.readLine());
+        } catch (NumberFormatException e) {
+            throw new MalformedSaveException();
+        } catch (IOException e) {
+            throw new IOException();
+        }
+        if (ticksElapsed < 0) {
+            throw new MalformedSaveException();
+        }
+        return ticksElapsed;
     }
 
     /**
@@ -55,14 +79,149 @@ public class ControlTowerInitialiser {
      * This method should call readAircraft(String).
      * @param reader reader from which to load the list of aircraft
      * @return list of aircraft read from the reader
-     * @throws IOException if the format of the text read from the reader is invalid according to
-     * the rules above
-     * @throws MalformedSaveException if an IOException is encountered when reading from the reader
+     * @throws IOException if an IOException is encountered when reading from the reader
+     * @throws MalformedSaveException if the format of the text read from the reader is invalid
+     * according to the rules above
      */
     public static List<Aircraft> loadAircraft(Reader reader) throws IOException,
             MalformedSaveException {
+        if (reader == null) {
+            throw new IOException();
+        }
+        int numAircraft = 0; //number of aircrafts expected
+        int numAircraftRead = 0; //number of aircrafts read from file
+        List<Aircraft> aircrafts = new ArrayList<>(); //list of aircrafts to be returend
+        BufferedReader file = new BufferedReader(reader);
+        String aircraftEncoded;
+        try {
+            numAircraft = Integer.parseInt(file.readLine());
+            if (numAircraft > 0) {
+                while (((aircraftEncoded = file.readLine()) != null)) {
+                    numAircraftRead++;
+                    aircrafts.add(readAircraft(aircraftEncoded));
+                }
+            }
+        } catch (NumberFormatException e) {
+            throw new MalformedSaveException();
+        } catch (IOException e) {
+            throw new IOException();
+        }
 
-        return null;
+        if (numAircraft != numAircraftRead) {
+            throw new MalformedSaveException();
+        }
+        return aircrafts;
+    }
+
+    /**
+     * Reads an aircraft from its encoded representation in the given string.
+     * If the AircraftCharacteristics.passengerCapacity of the encoded aircraft is greater than
+     * zero,  then a PassengerAircraft should be created and returned. Otherwise, a
+     * FreightAircraft  should be created and returned.
+     *
+     * The format of the string should match the encoded representation of an aircraft, as
+     * described  in Aircraft.encode().
+     *
+     * The encoded string is invalid if any of the following conditions are true:
+     *
+     * More/fewer colons (:) are detected in the string than expected.
+     * The aircraft's AircraftCharacteristics is not valid, i.e. it is not one of those listed in
+     * AircraftCharacteristics.values().
+     * The aircraft's fuel amount is not a double (i.e. cannot be parsed by Double.parseDouble(String)).
+     * The aircraft's fuel amount is less than zero or greater than the aircraft's maximum fuel capacity.
+     * The amount of cargo (freight/passengers) onboard the aircraft is not an integer (i.e.
+     * cannot  be parsed by Integer.parseInt(String)).
+     * The amount of cargo (freight/passengers) onboard the aircraft is less than zero or greater
+     * than the aircraft's maximum freight/passenger capacity.
+     * Any of the conditions listed in the Javadoc for readTaskList(String) are true.
+     * @param line line of text containing the encoded aircraft
+     * @return decoded aircraft instance
+     * @throws MalformedSaveException if the format of the given string is invalid according  to
+     * the rules above
+     */
+    public static Aircraft readAircraft (String line) throws MalformedSaveException {
+        String callsign;
+        AircraftCharacteristics aircraftCharacteristics;
+        TaskList taskListEncoded;
+        Double fuelAmount;
+        String emergencyStatus;
+        int cargo;
+        Aircraft aircraftRead;
+        String [] aircraftProperties = line.split(":");
+        if (aircraftProperties.length != 6) {
+            throw new MalformedSaveException();
+        }
+        try {
+            callsign = aircraftProperties[0];
+            aircraftCharacteristics = AircraftCharacteristics.valueOf(aircraftProperties[1]);
+            taskListEncoded = readTaskList(aircraftProperties[2]);
+            fuelAmount = Double.parseDouble(aircraftProperties[3]);
+            emergencyStatus = aircraftProperties[4];
+            cargo = Integer.parseInt(aircraftProperties[5]);
+            if (cargo > 0) {
+                aircraftRead = new PassengerAircraft(callsign, aircraftCharacteristics, taskListEncoded, fuelAmount,
+                        cargo);
+            }
+            else {
+                aircraftRead = new FreightAircraft(callsign, aircraftCharacteristics, taskListEncoded, fuelAmount,0);
+            }
+
+        } catch (IllegalArgumentException e) {
+            throw new MalformedSaveException();
+        }
+        return aircraftRead;
+    }
+
+
+    /**
+     *
+     * @param taskListPart
+     * @return
+     * @throws MalformedSaveException
+     */
+    public static TaskList readTaskList (String taskListPart) throws MalformedSaveException {
+        List<Task> taskList = new ArrayList<>();
+        String [] task = taskListPart.split(",");
+        for (String taskString : task) {
+            if (taskString.contains("@")) {
+                // check if taskString contains multiple '@'
+                String [] taskLoad = taskString.split("@");
+                if (taskLoad.length > 2) {
+                    throw new MalformedSaveException();
+                }
+                try {
+                    int loadPercent = Integer.parseInt(taskLoad[1]);
+                    taskList.add(new Task(TaskType.valueOf(taskLoad[0]),loadPercent));
+                    if (loadPercent < 0) {
+                        throw new IllegalArgumentException();
+                    }
+                } catch (IllegalArgumentException e) {
+                    throw new MalformedSaveException();
+                }
+            }
+            taskList.add(new Task(TaskType.valueOf(taskString)));
+        }
+        return new TaskList(taskList);
+    }
+
+    public static void readQueue (BufferedReader reader, List<Aircraft> aircraft,
+                                  AircraftQueue queue) throws IOException, MalformedSaveException {
+
+    }
+
+    public static void readLoadingAircraft (BufferedReader reader, List<Aircraft> aircraft,
+                                            Map<Aircraft, Integer> loadingAircraft) throws IOException, MalformedSaveException {
+
+    }
+
+    public static Terminal readTerminal (String line, BufferedReader reader,
+                                         List<Aircraft> aircraft) throws IOException,
+            MalformedSaveException {
+
+    }
+
+    public static Gate readGate (String line, List<Aircraft> aircraft) throws MalformedSaveException {
+
     }
 
     /**
@@ -100,20 +259,26 @@ public class ControlTowerInitialiser {
 
     /**
      * Loads the list of terminals and their gates from the given reader instance.
-     * The contents of the reader should match the format specified in the terminalsWithGatesWriter row of in the table shown in ViewModel.saveAs().
+     * The contents of the reader should match the format specified in the
+     * terminalsWithGatesWriter  row of in the table shown in ViewModel.saveAs().
      *
-     * For an example of valid queues reader contents, see the provided saves/terminalsWithGates_basic.txt and saves/terminalsWithGates_default.txt files.
+     * For an example of valid queues reader contents, see the provided
+     * saves/terminalsWithGates_basic.txt and saves/terminalsWithGates_default.txt files.
      *
      * The contents read from the reader are invalid if any of the following conditions are true:
      *
-     * The number of terminals specified at the top of the file is not an integer (i.e. cannot be parsed by Integer.parseInt(String)).
-     * The number of terminals specified is not equal to the number of terminals actually read from the reader.
-     * Any of the conditions listed in the Javadoc for readTerminal(String, BufferedReader, List) and readGate(String, List) are true.
+     * The number of terminals specified at the top of the file is not an integer  (i.e. cannot
+     * be parsed by Integer.parseInt(String)).
+     * The number of terminals specified is not equal to the number of terminals actually read
+     * from the reader.
+     * Any of the conditions listed in the Javadoc for readTerminal(String, BufferedReader, List)
+     * and readGate(String, List) are true.
      * This method should call readTerminal(String, BufferedReader, List).
      * @param reader reader from which to load the list of terminals and their gates
      * @param aircraft list of all aircraft, used when validating that callsigns exist
      * @return list of terminals (with their gates) read from the reader
-     * @throws MalformedSaveException if the format of the text read from the reader is invalid according to the rules above
+     * @throws MalformedSaveException if the format of the text read from the reader is invalid
+     * according to the rules above
      * @throws IOException if an IOException is encountered when reading from the reader
      */
     public static List<Terminal> loadTerminalsWithGates(Reader reader, List<Aircraft> aircraft) throws MalformedSaveException, IOException {
@@ -123,7 +288,8 @@ public class ControlTowerInitialiser {
 
     /**
      * Creates a control tower instance by reading various airport entities from the given readers.
-     * The following methods should be called in this order, and their results stored temporarily, to load information from the readers:
+     * The following methods should be called in this order, and their results stored
+     * temporarily,  to load information from the readers:
      *
      * loadTick(Reader) to load the number of elapsed ticks
      * loadAircraft(Reader) to load the list of all aircraft
@@ -131,13 +297,16 @@ public class ControlTowerInitialiser {
      * loadQueues(Reader, List, TakeoffQueue, LandingQueue, Map) to load the takeoff queue,
      * landing  queue and map of loading aircraft to their loading time remaining
      * Note: before calling loadQueues(), an empty takeoff queue and landing queue should be
-     * created  by calling their respective constructors. Additionally, an empty map should be created by calling:
+     * created  by calling their respective constructors. Additionally, an empty map should be
+     * created by calling:
      *
      * new TreeMap<>(Comparator.comparing(Aircraft::getCallsign))
-     * This is important as it will ensure that the map is ordered by aircraft callsign (lexicographically).
+     * This is important as it will ensure that the map is ordered by aircraft callsign
+     * (lexicographically).
      * Once all information has been read from the readers, a new control tower should be
      * initialised  by calling ControlTower(long, List, LandingQueue, TakeoffQueue, Map).
-     * Finally,  the terminals that have been read should be added to the control tower by calling ControlTower.addTerminal(Terminal).
+     * Finally,  the terminals that have been read should be added to the control tower by
+     * calling  ControlTower.addTerminal(Terminal).
      * @param tick reader from which to load the number of ticks elapsed
      * @param aircraft reader from which to load the list of aircraft
      * @param queues reader from which to load the aircraft queues and map of loading aircraft
@@ -152,9 +321,4 @@ public class ControlTowerInitialiser {
 
         return null;
     }
-
-
-
-
-
 }
---
+++ test/towersim/control/ControlTowerInitialiserTest.java
@@ -0,0 +1,30 @@
+package towersim.control;
+
+import org.junit.Before;
+import org.junit.Test;
+import towersim.aircraft.Aircraft;
+import towersim.aircraft.PassengerAircraft;
+
+import static org.junit.Assert.*;
+
+public class ControlTowerInitialiserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void loadAircraft() {
+
+    }
+
+    @Test
+    public void readAircraft() {
+
+    }
+
+    @Test
+    public void readTaskList() {
+
+    }
+}
